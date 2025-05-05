25221b3d74c6b3bf0b69204821baf0cbd4393b91
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-05-05 14:04:13 -0500

    Add templates

---
+++ students_db/db.py
@@ -0,0 +1,3 @@
+# Database code will go here.
+
+import sqlite3
---
+++ students_db/main.py
@@ -0,0 +1,8 @@
+# Programmer: Austin Long
+# Date: 2025-05-05
+# Program: Students DB
+
+# Import database. Make sure you run python from the root of the repository.
+import db
+
+# GUI code will go here...
